0eb1c2197d79a96171d7bb82c37a128e30f104b6
MauriceLee <ee.maurice.lee@gmail.com>
2019-02-18 21:12:42 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -9,7 +9,11 @@ while True:
     p.append(price)
     # p = [name, price]
     products.append(p)
-    #products.append([name, price])
+    # products.append([name, price])
 print(products)
 
-print(products[0][0])
+# print(products[0][0])
+
+for p in products:
+    print(p)
+    print(p[0], '的價格是', p[1])
